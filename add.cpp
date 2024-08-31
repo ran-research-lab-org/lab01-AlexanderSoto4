899c4187cf690c8d17639a7920da7504163f256d
HCarbia <heriberto.carbia@upr.edu>
2024-08-31 16:36:34 -0400

    graded with comments...

--- add.cpp
+++ add.cpp
@@ -1,5 +1,5 @@
 
-// (Alexander Soto)
+// (Alexander Soto)                     
 // This program adds two numbers
 
 int main() {
@@ -8,7 +8,13 @@ int main() {
   number2 = 13;
   sum = number1 + number2;
   std::cout << "The sum of these two integers is "
-            << sum << std::endl;
+            << sum << std::endl;              
   return 0;
 }
 
+
+
+//// 
+//// 5/10
+//// Programa no compila. (-5 pts)
+//// error en linea 11.
